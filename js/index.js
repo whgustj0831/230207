4a5f8f2cf65660b1b2bbfff9791271dc1e6357f0
whgustj0831 <122263057+whgustj0831@users.noreply.github.com>
2023-02-14 15:29:24 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $('.article1 .slide_group').slick({
     autoplaySpeed:3000,
     dots:true,
     arrows:true,
+    pauseOnFocus:false,
     prevArrow:'<button class="slick-prev slick-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
     nextArrow:'<button class="slick-next slick-arrow"><i class="fa-solid fa-chevron-right"></i></button>',
 })
@@ -13,6 +14,7 @@ $('.slide_group1').slick({
     prevArrow:false,
     nextArrow:false,
     centerMode:true,
+    pauseOnFocus:false,
     responsive:[{ 
         breakpoint:1025,
         settings:{
